8bf39f4fb3e1c849f85c9b58497a6a9699075287
ChengYao Wu <chengyaowu@ChengYaodeMacBook-Air.local>
2021-12-14 20:15:32 +0800

    new

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="就是紋繡"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
