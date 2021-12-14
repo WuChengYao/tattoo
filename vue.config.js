06ac65dc0c4b1403e38c47ec5baac499bbe6b67a
ChengYao Wu <chengyaowu@ChengYaodeMacBook-Air.local>
2021-12-14 20:34:25 +0800

    1

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // chainWebpack: config => {
+    //     config.module.rules.delete('eslint');
+    // },
+    // chainWebpack: config => {
+    //     config.resolve.alias.set(
+    //       'vue$',
+    //       path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+    //     )
+    // },
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: '紋繡'
+        }
+    },
+    publicPath: './'
+}
