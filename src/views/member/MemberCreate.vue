b096031aca93b5272311490bf8017a32d10a1266
ChengYao Wu <chengyaowu@ChengYaodeMacBook-Air.local>
2021-12-16 23:10:14 +0800

    增加一點點

--- src/views/member/MemberCreate.vue
+++ src/views/member/MemberCreate.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="MemberCreate">
-      <b-container>
+    <b-container>
         <h2>新增會員</h2>
         <!-- 基本資料 -->
         <b-row class="mb-2">
@@ -25,13 +25,13 @@
         <!-- 膚質狀況 -->
         <b-row class="mb-2">
             <b-col sm="7">
-                <b-form-group label="膚質狀況" v-slot="{ariaDescribedby}">
+                <b-form-group label="膚質狀況" v-slot="{skin_condition}">
                     <b-form-checkbox-group
-                        id="checkbox-group-1"
+                        id="skin_condition"
                         v-model="skinCondition"
                         :options="skinConditionOptions"
-                        :aria-describedby="ariaDescribedby"
-                        name="flavour-1"
+                        :aria-describedby="skin_condition"
+                        name="skin_condition"
                     ></b-form-checkbox-group>
                 </b-form-group>
             </b-col>
@@ -44,13 +44,13 @@
         <!-- 身體狀況 -->
         <b-row class="mb-2">
             <b-col sm="7">
-                <b-form-group label="身體狀況" v-slot="{ariaDescribedby}">
+                <b-form-group label="身體狀況" v-slot="{user_fettle}">
                     <b-form-checkbox-group
-                        id="checkbox-group-1"
+                        id="user_fettle"
                         v-model="userFettle"
                         :options="userFettleOptions"
-                        :aria-describedby="ariaDescribedby"
-                        name="flavour-1"
+                        :aria-describedby="user_fettle"
+                        name="user_fettle"
                     ></b-form-checkbox-group>
                 </b-form-group>
             </b-col>
@@ -59,7 +59,43 @@
                 <b-form-input v-model.trim="userFettleNarrate" placeholder="其他敘述" type="text"></b-form-input>
             </b-col>
         </b-row>
-      </b-container>
+        <hr>
+        <!-- 現有服用過藥物 -->
+        <b-row class="mb-2">
+            <b-col sm="12">
+                <b-form-group label="現有服用過藥物" v-slot="{ take_medicine }">
+                    <b-form-radio v-model="takeMedicine" :aria-describedby="take_medicine" name="some-radios" value="nDrug">無</b-form-radio>
+                    <b-form-radio v-model="takeMedicine" :aria-describedby="take_medicine" name="some-radios" value="yDrug">有</b-form-radio>
+                    <b-form-input v-show="takeMedicine == 'yDrug'" v-model.trim="drugName" placeholder="藥名" type="text"></b-form-input>
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <hr>
+        <!-- 曾經有做過項目經驗 -->
+        <b-row class="mb-2">
+            <b-col sm="12">
+                曾經有做過項目經驗
+            </b-col>
+        </b-row>
+        <hr>
+        <!-- 臉部疾病 -->
+        <b-row class="mb-2">
+            <b-col sm="12">
+                <b-form-group label="有無其他臉部、眼睛、唇部疾病或損害等情況需說明" v-slot="{ face_wound }">
+                    <b-form-radio v-model="hasFaceWound" :aria-describedby="face_wound" name="some-radios" value="notHave">無</b-form-radio>
+                    <b-form-radio v-model="hasFaceWound" :aria-describedby="face_wound" name="some-radios" value="have">有</b-form-radio>
+                    <b-form-input v-show="hasFaceWound == 'have'" v-model.trim="faceWoundName" placeholder="請描述..." type="text"></b-form-input>
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <hr>
+        <!-- 如何得知本店 -->
+        <b-row class="mb-2">
+            <b-col sm="12">
+                如何得知本店
+            </b-col>
+        </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -73,7 +109,7 @@ export default {
             userName: '',
             userPhone: '',
             userLine: '',
-            userBirthday: '',
+            userBirthday: '2021/01/01',
             //膚質狀況
             skinCondition: [], // Must be an array reference!
             skinConditionOptions: [
@@ -98,7 +134,12 @@ export default {
             ],
             userFettleNarrate: '',
             //曾有服用過藥物
-            //臉部傷口
+            takeMedicine: '',
+            drugName: '',
+            //曾經有做過項目經驗
+            //臉部疾病
+            hasFaceWound: '',
+            faceWoundName: '',
             //如何得知本店
         }
     },
